36d6e44d1086ad4d6ac4a42e0dfbd15bc69ad2ec
Dika Auliady <auliady.dika@gmail.com>
2022-11-10 21:36:51 +0700

    add method and assert

--- src/test/java/com/technical/steps/SelectMenuSteps.java
+++ src/test/java/com/technical/steps/SelectMenuSteps.java
@@ -5,15 +5,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
-import static com.technical.utils.WebDriverUtils.getDriver;
-
+import static com.technical.utils.WebDriverUtils.*;
 
 public class SelectMenuSteps {
-    private static WebDriver driver;
     @Given("User go to {string}")
     public void userGoTo(String website){
         driver = getDriver();
@@ -22,33 +20,43 @@ public class SelectMenuSteps {
 
     @When("User in {string} page")
     public void userInPage(String page) {
-        WebElement selectMenu = driver.findElement(By.xpath("//*[text()='Select Menu']"));
-        selectMenu.isDisplayed();
-        Assert.assertEquals(selectMenu.getText(),page);
+        driver.findElement(By.xpath("//*[text()='Select Menu']")).isDisplayed();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("window.scrollBy(0,250)","");
     }
 
     @And("User choose select value {string}")
     public void userChooseSelectValue(String menu1) {
-        System.out.println(menu1);
+        driver.findElement(By.id("withOptGroup")).click();
+        driver.findElement(By.id("react-select-2-option-3")).click();
     }
 
     @And("User choose select one {string}")
     public void userChooseSelectOne(String menu2) {
-        System.out.println(menu2);
+        driver.findElement(By.id("selectOne")).click();
+        driver.findElement(By.id("react-select-3-option-0-5")).click();
     }
 
     @And("User choose old style select menu {string}")
     public void userChooseOldStyleSelectMenu(String menu3) {
-        System.out.println(menu3);
+        Select oldStyle = new Select(driver.findElement(By.id("oldSelectMenu")));
+        oldStyle.selectByValue("10");
     }
 
     @And("User choose multi select drop down “all color”")
     public void userChooseMultiSelectDropDownAllColor() {
-        System.out.println("all color");
+        driver.findElement(By.xpath("//*[text()='Select...']")).click();
+        driver.findElement(By.id("react-select-4-option-0")).click();
+        driver.findElement(By.id("react-select-4-option-1")).click();
+        driver.findElement(By.id("react-select-4-option-2")).click();
+        driver.findElement(By.id("react-select-4-option-3")).click();
     }
 
     @Then("User success input all select menu")
     public void userSuccessInputAllSelectMenu() {
-        System.out.println("all menu");
+        String getActualMenu1 = driver.findElement(By.xpath("//*[@id='withOptGroup']//child::div/div/div")).getText();
+        Assert.assertEquals(getActualMenu1,"Another root option");
+        driver.close();
+        driver.quit();
     }
 }
